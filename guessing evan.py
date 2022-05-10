c841c657d22d301661bd88a32ab7c80019f07051
Sclurbs <105256652+Sclurbs@users.noreply.github.com>
2022-05-09 19:58:25 -0700

    Update guessing evan.py

--- guessing evan.py
+++ guessing evan.py
@@ -1,20 +1,22 @@
 
 
-def main():  # defines the area in indents that will be triggered with main()#
-    print('hi')
-    print("hi")
 
+you = "spongebob quarepants"
 question = ""
 yes = 0
 no = 1
 ready = False
 
+def main():
+    print("mother fucker")
+    print("You don't get another chance!")
+    print("this opportunity comes once in a lifetime")
 
 while ready == False:
     if yes < no:
         print("It would seem that you are not ready!")
         question = input("are you ready now?")
-        if question == ("yes"):
+        if question == ("im ready"):
             yes += 1
             no = 0
     else:
@@ -33,7 +35,7 @@ while ready == True:
             print("you are finally ready!")
             question = input("Are you still ready?")
             ready_combo_meter += 1
-        if question != ("yes"):
+        if question != ("im ready"):
             yes = 0
 
     else:
@@ -43,12 +45,11 @@ while ready == True:
 
 
 
-print("you aren't ready again!")
-loop_back = input('LOOP ')
+print("whoops you aren't ready again!")
+loop_back = input('want to try to be ready again? ')
 
-if loop_back == ("yes"):
+while loop_back == ("im ready"):
     main()
 
+print("you weren't ready anyway")
 
-
-main()
